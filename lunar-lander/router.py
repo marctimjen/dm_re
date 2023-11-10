7c317301aec39cebfe5a5056892419718d139494
marctimjen <79468558+marctimjen@users.noreply.github.com>
2023-11-10 14:54:20 +0100

    update router and upload the best model

--- lunar-lander/router.py
+++ lunar-lander/router.py
@@ -2,23 +2,39 @@ import numpy as np
 from fastapi import APIRouter
 from models.dtos import LunarLanderPredictRequestDto, LunarLanderPredictResponseDto
 
+import gymnasium as gym
+from stable_baselines3 import A2C
+
+env = gym.make(
+    "LunarLander-v2",
+    continuous=False,
+    gravity=-10.0,
+    enable_wind=False,
+    wind_power=15.0,
+    turbulence_power=1.5,
+    render_mode="human"
+)
+
+models_dir = "models/A2C"
+model_path = f"{models_dir}/240000.zip"
+
+model = A2C.load(model_path, env=env)
+
+
+
 
 router = APIRouter()
 
 
 @router.post('/predict', response_model=LunarLanderPredictResponseDto)
 def predict(request: LunarLanderPredictRequestDto):
-    # x, y, vx, vy, theta, vtheta, l1, l2 = oberservation = request.observation
-    # reward = request.reward
-    # total_reward = request.total_reward
-    # game_ticks = request.game_ticks
-    # lander_chrashed = request.is_terminal
+    obs = request.observation
 
     if request.is_terminal:
         print("Current game is over, a new game will start with next request!")
 
     # Your moves go here!
-    action = int(np.random.randint(4))
+    action, _ = model.predict(obs)
 
     return LunarLanderPredictResponseDto(
         action=action
