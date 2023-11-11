9842d9edf1114f6aaa83efd2efd624e79ab4a46d
marctimjen <79468558+marctimjen@users.noreply.github.com>
2023-11-11 14:35:05 +0100

    Update router.py

--- lunar-lander/router.py
+++ lunar-lander/router.py
@@ -16,14 +16,14 @@ env = gym.make(
     render_mode="human"
 )
 
-models_dir = "/home/paperspace/dm_re/models/A2C"
-model_path = f"{models_dir}/240000.zip"
-print(model_path)
-model = A2C.load(model_path, env=env)
+# models_dir = "/home/paperspace/dm_re/models/A2C"
+# model_path = f"{models_dir}/240000.zip"
+# print(model_path)
+# model = A2C.load(model_path, env=env)
 
-# models_dir = "/home/paperspace/dm_re/models/PPO"
-# model_path = f"{models_dir}/300000.zip"
-# model = PPO.load(model_path, env=env)
+models_dir = "/home/paperspace/dm_re/models/PPO"
+model_path = f"{models_dir}/300000.zip"
+model = PPO.load(model_path, env=env)
 
 # models_dir = "/home/paperspace/dm_re/models/PPO"
 # model_path = f"{models_dir}/1000000.zip"
@@ -35,14 +35,14 @@ model = A2C.load(model_path, env=env)
 
 router = APIRouter()
 
-
+start_time = time.time()
 @router.post('/predict', response_model=LunarLanderPredictResponseDto)
 def predict(request: LunarLanderPredictRequestDto):
     obs = request.observation
 
     if request.is_terminal:
         with open("validation_attempt.log", "a+") as f:
-            f.write(f"{time.time()}: Ending game!")
+            f.write(f"{time.time()- start_time}: Ending game! \n")
         print("Current game is over, a new game will start with next request!")
 
 
