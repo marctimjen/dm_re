2a50ec74b7fb85a267a8219f733117211fa515d2
marctimjen <79468558+marctimjen@users.noreply.github.com>
2023-11-11 14:29:01 +0100

    entire path

--- lunar-lander/router.py
+++ lunar-lander/router.py
@@ -16,20 +16,20 @@ env = gym.make(
     render_mode="human"
 )
 
-models_dir = "models/A2C"
+models_dir = "/home/paperspace/dm_re/models/A2C"
 model_path = f"{models_dir}/240000.zip"
 print(model_path)
 model = A2C.load(model_path, env=env)
 
-# models_dir = "models/PPO"
+# models_dir = "/home/paperspace/dm_re/models/PPO"
 # model_path = f"{models_dir}/300000.zip"
 # model = PPO.load(model_path, env=env)
 
-# models_dir = "models/PPO"
+# models_dir = "/home/paperspace/dm_re/models/PPO"
 # model_path = f"{models_dir}/1000000.zip"
 # model = PPO.load(model_path, env=env)
 
-# models_dir = "models/DQN"
+# models_dir = "/home/paperspace/dm_re/models/DQN"
 # model_path = f"{models_dir}/18000000.zip"
 # model = DQN.load(model_path, env=env)
 
