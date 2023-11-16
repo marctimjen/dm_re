a54018a874d9b2f5c6e2d56bd32a6a15832093db
marctimjen <79468558+marctimjen@users.noreply.github.com>
2023-11-16 09:43:44 +0100

    remove the last ars

--- trainer/delete_ars.py
+++ trainer/delete_ars.py
@@ -3,27 +3,29 @@ import pandas
 import numpy as np
 import os
 
-cv = int(167)
+cv_list = [int(i) for i in [161, 159, 158, ]]
 
-with open("NEPTUNE_API_TOKEN.txt", "r") as file:
-    # Read the entire content of the file into a string
-    token = file.read()
 
-run = neptune.init_run(
-    project="Kernel-bois/reinforcement-learning",
-    api_token=token,
-    with_id=f"REIN-{cv}",
-)
+for cv in cv_list:
+    with open("NEPTUNE_API_TOKEN.txt", "r") as file:
+        # Read the entire content of the file into a string
+        token = file.read()
 
-m = run["reward/mean_reward"]
-df = m.fetch_values()
+    run = neptune.init_run(
+        project="Kernel-bois/reinforcement-learning",
+        api_token=token,
+        with_id=f"REIN-{cv}",
+    )
 
-delete_names = np.array(df.step[280 < df.value][df.value < 290]).astype(int)
+    m = run["reward/mean_reward"]
+    df = m.fetch_values()
 
-pa = "/home/tyson/dm_re/trainer/models"
+    delete_names = np.array(df.step[280 < df.value][df.value < 290]).astype(int)
 
-for i in delete_names:
-    path = pa + f"/ARS_REIN-{cv}_EP_" + str(i) + ".zip"
-    os.remove(path)
+    pa = "/home/tyson/dm_re/trainer/models"
 
-run.stop()
+    for i in delete_names:
+        path = pa + f"/ARS_REIN-{cv}_EP_" + str(i) + ".zip"
+        os.remove(path)
+
+    run.stop()
