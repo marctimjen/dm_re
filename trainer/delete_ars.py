d367b061cc3488ff156381664e379e604b3f7e12
marctimjen <79468558+marctimjen@users.noreply.github.com>
2023-11-16 09:40:15 +0100

    try delete again

--- trainer/delete_ars.py
+++ trainer/delete_ars.py
@@ -18,12 +18,12 @@ run = neptune.init_run(
 m = run["reward/mean_reward"]
 df = m.fetch_values()
 
-delete_names = np.array(df.step[df.value < 289]).astype(int)
+delete_names = np.array(df.step[280 < df.value][df.value < 290]).astype(int)
 
 pa = "/home/tyson/dm_re/trainer/models"
-
 
 for i in delete_names:
     path = pa + f"/ARS_REIN-{cv}_EP_" + str(i) + ".zip"
     os.remove(path)
 
+run.stop()
